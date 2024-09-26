ca6f8a169df5316d533a40c3cdb6abeb17b34d96
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-09-26 21:44:26 +0200

    impresión de mensajes

---
+++ src/conceptosbasicoskotlin/ImpresionMensajes.kt
@@ -0,0 +1,21 @@
+/*
+¿Puedes escribir una función main() que imprima estos mensajes en cuatro líneas separadas?
+
+Use the val keyword when the value doesn't change.
+Use the var keyword when the value can change.
+When you define a function, you define the parameters that can be passed to it.
+When you call a function, you pass arguments for the parameters.
+
+ */
+
+package conceptosbasicoskotlin
+
+class ImpresionMensajes {
+}
+
+fun main() {
+    println("Use the val keyword when the value doesn't change.")
+    println("Use the var keyword when the value can change.")
+    println("When you define a function, you define the parameters that can be passed to it.")
+    println("When you call a function, you pass arguments for the parameters.")
+}
